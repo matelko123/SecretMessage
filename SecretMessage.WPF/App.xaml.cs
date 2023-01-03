bc7aa986d2c97fae8034de7880e49f3a5a2f9b8e
matelko <mateusz280798@gmail.com>
2023-01-03 22:31:42 +0100

    Firebase connected.

---
+++ SecretMessage.WPF/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using Firebase.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SecretMessage.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+        
+        public App()
+        {
+            _host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+                    string firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY") 
+                                            ?? throw new ArgumentNullException("FIREBASE_API_KEY");
+
+                    service.AddSingleton(new FirebaseAuthProvider(new FirebaseConfig(firebaseApiKey)));
+                    
+                    service.AddSingleton<MainWindow>((services) => new MainWindow());
+                })
+                .Build();
+        }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
